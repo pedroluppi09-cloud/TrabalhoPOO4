d74a9bb9ecd608dfc59ad51444472cc8cf1eff9f
Usuario <pedroluppi.09@gmail.com>
2026-08-28 10:42:08 -0300

    a

--- TrabalhoPOO4/src/Main.java
+++ TrabalhoPOO4/src/Main.java
@@ -11,5 +11,7 @@ public class Main {
         System.out.println("jkgahbvuauiofhxui");
 
         System.out.println("Bom dia, Boa tarde, Boa noite");
+
+        System.out.println("Leleleu");
     }
 }
