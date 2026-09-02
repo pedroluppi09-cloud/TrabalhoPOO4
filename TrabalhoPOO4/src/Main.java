7acad4be368fac1ef1c98bec16b16443f6a28a54
Usuario <pedroluppi.09@gmail.com>
2026-09-02 10:17:51 -0300

    Init Usuários

--- TrabalhoPOO4/src/Main.java
+++ TrabalhoPOO4/src/Main.java
@@ -1,5 +1,13 @@
+import negocio.Usuario;
+
 public class Main {
     static void main() {
+        // inicia com 2 usuários
+        Usuario Ouvinte = new Usuario('O');
+        Usuario Admin = new Usuario('A');
 
+        System.out.println("Usuários cadastrados (nome, senha): ");
+        Ouvinte.imprimir();
+        Admin.imprimir();
     }
 }
--- TrabalhoPOO4/src/negocio/Usuario.java
+++ TrabalhoPOO4/src/negocio/Usuario.java
@@ -4,4 +4,28 @@ public class Usuario {
     private String nome;
     private char funcao;
     private String senha;
+
+    public Usuario(char funcao) {
+        if (funcao == 'O'){
+            this.nome = "Ouvinte";
+            this.funcao = funcao;
+            this.senha = "1234";
+        }
+
+        if (funcao == 'A'){
+            this.nome = "Admin";
+            this.funcao = funcao;
+            this.senha = "5678";
+        }
+    }
+
+    public void imprimir() {
+        if (funcao == 'A') {
+            System.out.println("Administrador: " + nome + ", " + senha);
+        }
+
+        if (funcao == 'O') {
+            System.out.println("Ouvinte: " + nome + ", " + senha);
+        }
+    }
 }
