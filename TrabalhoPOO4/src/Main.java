392c0390ba1c8048b6128350807c599b342ad58b
Usuario <pedroluppi.09@gmail.com>
2026-09-02 09:55:25 -0300

    Classes de Entidade

--- TrabalhoPOO4/src/Main.java
+++ TrabalhoPOO4/src/Main.java
@@ -1,21 +1,5 @@
 public class Main {
     static void main() {
-        System.out.println("Olá mundo");
-        int c = 3;
-        int a = 2;
-        int d = 4;
 
-        System.out.println(c);
-
-        int b = 2;
-
-        int r = a*b;
-
-        System.out.println(Math.pow(r, 2));
-        System.out.println("jkgahbvuauiofhxui");
-
-        System.out.println("Bom dia, Boa tarde, Boa noite");
-
-        System.out.println("Leleleu");
     }
 }
---
+++ TrabalhoPOO4/src/negocio/Genero.java
@@ -0,0 +1,4 @@
+package negocio;
+
+public enum Genero {
+}
