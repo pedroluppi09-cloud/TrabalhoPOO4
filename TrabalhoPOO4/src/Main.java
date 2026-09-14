d3b2d183c80ecea4d9d164f0c85b66892ff51485
miguelpassos333-crypto <miguelpassos333@gmail.com>
2026-09-14 08:57:39 -0300

    a

--- TrabalhoPOO4/src/Main.java
+++ TrabalhoPOO4/src/Main.java
@@ -28,17 +28,17 @@ public class Main {
 
         String[] nomes = {"Highway to Hell", "Bohemian Rhapsody", "The Thrill Is Gone", "Sweet Home Chicago", "Take Five", "What a Wonderful World",
                 "Thriller", "Shape of You", "Evidências", "Fio de Cabelo", "Lose Yourself", "Juicy", "Ninguém Explica Deus", "Aos Olhos do Pai", "Titanium", "Levels",
-                "Como Nossos Pais", "Aquele Abraço", "Master of Puppets", "The Number of the Beast"};
+                "Como Nossos Pais", "Aquele Abraço", "Master of Puppets", "The Number of the Beast", "Without Me"};
 
         String[] artista = {"AC/DC", "Queen", "B.B. King", "Buddy Guy", "Dave Brubeck", "Louis Armstrong", "Michael Jackson", "Ed Sheeran", "Chitãozinho & Xororó",
                 "Chitãozinho & Xororó", "Eminem", "The Notorious B.I.G.", "Preto no Branco", "Diante do Trono", "David Guetta", "Avicii", "Elis Regina", "Gilberto Gil",
-                "Metallica", "Iron Maiden"};
+                "Metallica", "Iron Maiden", "Eminem"};
 
         Genero[] genero = {Genero.ROCK, Genero.BLUES, Genero.JAZZ, Genero.POP, Genero.SERTANEJO, Genero.HIPHOP, Genero.GOSPEL, Genero.ELETRO, Genero.MPB, Genero.HEAVYMETAL};
 
         boolean adicionou;
 
-        for(int i = 0; i < 20; i++){
+        for(int i = 0; i < 21; i++){
             M = Musica.getInstance(nomes[i], artista[i], genero[i/2]);
             adicionou = S.adicionarMusica(M);
 
